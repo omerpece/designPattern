430d1174c71dd2bb00b24b4fe726d2dd0e2692d4
muratvuranok <muratvuranok@hotmail.com>
2019-08-28 13:28:01 +0300

    Add project files.

---
+++ WFA_DesignPatterns.RepositoryPattern/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace WFA_DesignPatterns.RepositoryPattern
+{
+    public interface IRepository<TEntity, TId>
+        where TEntity : class
+            where TId : struct
+    {
+        IQueryable<TEntity> FindAll();
+        TEntity FindSingle(Expression<Func<TEntity, bool>> expression);
+        TEntity FindSingle(TId id);
+        void Insert(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
---
+++ WFA_DesignPatterns.RepositoryPattern/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace WFA_DesignPatterns.RepositoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
